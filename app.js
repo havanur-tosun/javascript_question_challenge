2cecf7fafe2acaa41ac22fad5da2612eec0f7094
Havanur <havanurtosun0618@gmail.comhavanurtosun0618@gmail.com>
2021-07-05 16:50:03 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+//QUESTION 1
+
+// Write a function that returns the string "something" joined with a space " " and the given argument a.
+
+// let x = 'something';
+// function addToStart (a) {
+//     return x.concat(" ", a)
+// }
+// console.log(addToStart("bob Jane"));
+
+//-------------------------------------------------------//
+
+//QUESTION-2
+
+// write a function
+// input: string => "21/06/2019"
+// output: string => "2019-06-21"
+// [12:18 PM] lets make it more generic and complex.
+// input: string => "21/06/2019" or "21.06.2019" or "21 06 2019" etc.
+// output: string => "2019-06-21"
+
