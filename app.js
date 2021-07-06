a8dc80226e438a43297bc00d2675e07281e4c2e3
Havanur <havanurtosun0618@gmail.comhavanurtosun0618@gmail.com>
2021-07-06 19:45:33 -0400

    commit

--- app.js
+++ app.js
@@ -19,3 +19,71 @@
 // input: string => "21/06/2019" or "21.06.2019" or "21 06 2019" etc.
 // output: string => "2019-06-21"
 
+//=============================================================//
+
+//Reverse String
+
+// 1st method
+
+// let aString = "codersclan";
+
+// let reversedString = "";
+
+// for (let i = aString.length - 1; i >= 0; i--) {
+//     reversedString += aString[i]
+// }
+
+// console.log(reversedString);
+
+
+//2nd method
+
+// reversedString = aString.split("").reverse().join("");
+
+// console.log(reversedString);
+
+// 3rd method 
+
+// function reverseString(str) {
+//     var newString = "";
+//     for (var i = str.length - 1; i >= 0; i--) {
+//         newString += str[i];
+//     }
+//     return newString;
+// }
+
+// console.log(reverseString('codersclan'));
+
+
+//=======================================================================//
+
+// fibonacci
+
+// function fibonacci(num){
+//     var a = 1, b = 0, temp;
+  
+//     while (num >= 0){
+//       temp = a;
+//       a = a + b;
+//       b = temp;
+//       num--;
+//     }
+  
+//     return b;
+//   }
+
+// console.log(fibonacci(2));
+
+// const number = parseInt(prompt('Enter the number of terms: '));
+
+const number = 10;
+let n1 = 0, n2 = 1, nextTerm;
+
+console.log('Fibonacci Series:');
+
+for (let i = 1; i <= number; i++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+}
